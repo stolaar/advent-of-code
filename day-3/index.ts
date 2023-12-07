6765c4a9769a8ad22215a0a93834d18f5d87b815
Aleksandar Stolikj <stolaaar@gmail.com>
2023-12-07 23:00:35 +0100

    ref: optimize solution for day 3 part one

--- day-3/index.ts
+++ day-3/index.ts
@@ -5,28 +5,56 @@ export interface INumber {
 }
 
 export interface INumbersReducer {
-    numbers: INumber[]
     currentNumber: string
     sum: number
     start?: number
-    end?: number
+}
+const isVerticallyAdjacent = (start: number, end: number) => (_: string, index: number) => index >= start - 1 && index <= end + 1
+
+const partNumber = (number: INumber,currentLine: string,lineIndex: number, lines: string[]) => {
+    let checks = []
+    if (number.start > 0) {
+        checks.push(currentLine[number.start - 1])
+    }
+    if (number.end < currentLine.length - 1 && number.end >= 0) {
+        checks.push(currentLine[number.end + 1])
+    }
+    if (lineIndex > 0) {
+        const sliced = lines[lineIndex - 1].substring(number.start - 1, number.end + 2)
+        checks.push(...sliced)
+    }
+    if (lineIndex < lines.length - 1) {
+        const sliced = lines[lineIndex + 1].substring(number.start - 1, number.end + 2)
+        checks.push(...sliced)
+    }
+
+    const hasAdjacentSymbol = checks.some((value) => {
+        return !Number.isInteger(parseInt(value)) && value !== '.'
+    })
+
+    if (hasAdjacentSymbol) {
+        return number.value
+    }
+
+    return 0
 }
 
-const isVerticallyAdjacent = (start: number, end: number) => (_: string, index: number) => index >= start - 1 && index <= end + 1
+const initialNumbersReduce: INumbersReducer = {
+    currentNumber: '',
+    sum: 0,
+}
 
 export const partOne = (input: string[]) => {
     return input.reduce((acc, line, lineIndex, self) => {
-        const numbers = line.split('').reduce<INumbersReducer>((state, char, index, numbersArr) => {
+       return acc + line.split('').reduce<INumbersReducer>((state, char, index, numbersArr) => {
             if (Number.isInteger(parseInt(char))) {
                 if(index === numbersArr.length - 1) {
-                   return {
-                       ...state,
-                       numbers: [...state.numbers, {
-                           value: Number(`${state.currentNumber}${char}`),
-                           start: state.start ?? 0,
-                           end: index
-                       }],
-                   }
+                    const number ={
+                        value: Number(`${state.currentNumber}${char}`),
+                        start: state.start ?? 0,
+                        end: index
+                    }
+                    return { ...state, sum: state.sum + partNumber(number, line, lineIndex, self) }
                 }
                 return {
                     ...state,
@@ -35,49 +63,14 @@ export const partOne = (input: string[]) => {
                 }
             }
             if (state.currentNumber) {
-                return {
-                    ...state,
-                    numbers: [...state.numbers, {
+                return {...initialNumbersReduce, sum: state.sum + partNumber({
                         value: Number(state.currentNumber),
                         start: state.start ?? 0,
                         end: index - 1
-                    }],
-                    currentNumber: ''
-                }
+                    }, line, lineIndex, self) }
             }
-            return state
-        }, {numbers: [] as INumber[], currentNumber: '', sum: 0}).numbers
-
-        const sum = numbers.reduce((lineNumberAcc, number) => {
-            let checks = []
-            if (number.start > 0) {
-                checks.push(line[number.start - 1])
-            }
-            if (number.end < line.length - 1 && number.end >= 0) {
-                checks.push(line[number.end + 1])
-            }
-            if (lineIndex > 0) {
-                const previousLine = self[(lineIndex as number) - 1].split('')
-                checks.push(...previousLine.filter(isVerticallyAdjacent(number.start, number.end)))
-            }
-            if (lineIndex < self.length - 1) {
-                const nextLine = self[(lineIndex as number) + 1].split('')
-                checks.push(...nextLine.filter(isVerticallyAdjacent(number.start, number.end)))
-            }
-
-
-            const hasAdjacentSymbol = checks.some((value) => {
-                return !Number.isInteger(parseInt(value)) && value !== '.'
-            })
-
-            if (hasAdjacentSymbol) {
-                return lineNumberAcc + number.value
-            }
-
-
-            return lineNumberAcc
-        }, 0)
-        return acc + sum
+            return {...initialNumbersReduce, sum: state.sum}
+        }, initialNumbersReduce).sum
     }, 0)
 }
 
