13e9c1af5fd56df9ff5dd56a22ccad92e3cef0d3
Aleksandar Stolikj <stolaaar@gmail.com>
2023-12-11 23:27:15 +0100

    add prettier

--- main.ts
+++ main.ts
@@ -1,38 +1,44 @@
 import path from "path";
 import * as fs from "fs";
-import {ISolution} from "./types";
+import { ISolution } from "./types";
 
-const [,, day] = process.argv
+const [, , day] = process.argv;
 
-
-if(!Number.isInteger(parseInt(day))) {
-    throw new Error("Please provide the day")
+if (!Number.isInteger(parseInt(day))) {
+  throw new Error("Please provide the day");
 }
 
 const readMultilineInput = (inputPath: string) => {
-    const result = fs.readFileSync(inputPath, 'utf-8').split('\n')
-    result.pop()
+  const result = fs.readFileSync(inputPath, "utf-8").split("\n");
+  result.pop();
 
-    return result
-}
+  return result;
+};
 
-;(async () => {
-    const modulePath = path.resolve(__dirname,`day-${day}`)
-    const dayModule: ISolution = await import(path.resolve(modulePath, 'index.ts'))
-    const input = readMultilineInput(path.resolve(modulePath,"input.txt"))
+(async () => {
+  const modulePath = path.resolve(__dirname, `day-${day}`);
+  const dayModule: ISolution = await import(
+    path.resolve(modulePath, "index.ts")
+  );
+  const input = readMultilineInput(path.resolve(modulePath, "input.txt"));
 
-    const args: string[] = dayModule.processInput?.(input) || input
+  console.time("Input processing");
+  const args: string[] = dayModule.processInput?.(input) || input;
+  console.timeEnd("Input processing");
 
-    const partOneStart= performance.now()
-    const solutionPartOne = dayModule.partOne(args)
-    const partOneEnd = performance.now()
-    console.log("Part one solution", solutionPartOne)
-    console.log(`Part one execution time ${Math.round(partOneEnd - partOneStart)}ms`)
+  const partOneStart = performance.now();
+  const solutionPartOne = dayModule.partOne(args);
+  const partOneEnd = performance.now();
+  console.log("Part one solution", solutionPartOne);
+  console.log(
+    `Part one execution time ${Math.round(partOneEnd - partOneStart)}ms`,
+  );
 
-    const partTwoStart = performance.now()
-    const solutionPartTwo = dayModule.partTwo(args)
-    const partTwoEnd= performance.now()
-    console.log("Part two solution", solutionPartTwo)
-    console.log(`Part two execution time ${Math.round(partTwoEnd - partTwoStart)}ms`)
-
-})()
+  const partTwoStart = performance.now();
+  const solutionPartTwo = dayModule.partTwo(args);
+  const partTwoEnd = performance.now();
+  console.log("Part two solution", solutionPartTwo);
+  console.log(
+    `Part two execution time ${Math.round(partTwoEnd - partTwoStart)}ms`,
+  );
+})();
