c51cabbd1c5785d7488ceac89d6c4ea0f10bf019
Aleksandar Stolikj <stolaaar@gmail.com>
2023-12-11 23:53:24 +0100

    solve day six part one

---
+++ day-6/index.ts
@@ -0,0 +1,40 @@
+interface IRace {
+  duration: number
+  distance: number
+}
+
+export const processInput = (input: string[]) => {
+  const [timesLine, distancesLine] = input
+  const times = timesLine.split(": ")[1].split("  ").filter(Boolean).map(Number)
+  const distances = distancesLine
+    .split(": ")[1]
+    .split("  ")
+    .filter(Boolean)
+    .map(Number)
+  return times.map((time, index) => ({
+    duration: time,
+    distance: distances[index],
+  }))
+}
+
+const getNumOfWaysToBeatTheRecord = (duration: number, distance: number) => {
+  let results = []
+  for (let i = 1; i < duration; i++) {
+    if (i * (duration - i) > distance) {
+      results.push(i)
+    }
+  }
+  const first = results[0]
+  const last = results[results.length - 1]
+  return last - first + 1
+}
+
+export const partOne = (races: IRace[]) => {
+  return races
+    .map((race) => getNumOfWaysToBeatTheRecord(race.duration, race.distance))
+    .reduce((acc, curr) => acc * curr, 1)
+}
+
+export const partTwo = (input: string[]) => {
+  return ""
+}
