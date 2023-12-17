ced17488ddded6b25a7db7823010833bd90e4012
Aleksandar Stolikj <stolaaar@gmail.com>
2023-12-17 16:26:17 +0100

    solve day seven part two

--- day-7/index.ts
+++ day-7/index.ts
@@ -8,6 +8,14 @@ interface IHandValue {
   secondEvalStrengths: number[]
 }
 
+interface IEvaluatedHand extends IHand {
+  value: IHandValue
+}
+
+export interface IHandValueWithJokers extends IHandValue {
+  numberOfJokers: number
+}
+
 const cardStrengthsMap: Record<string | number, number> = {
   A: 14,
   K: 13,
@@ -16,7 +24,7 @@ const cardStrengthsMap: Record<string | number, number> = {
   T: 10,
 }
 
-const evaluateHand = (cards: string[]): IHandValue => {
+const getNumberOfMatches = (cards: string[]) => {
   const numberOfMatches = cards.reduce((acc, curr) => {
     acc.set(curr, (acc.get(curr) || 0) + 1)
     return acc
@@ -31,6 +39,21 @@ const evaluateHand = (cards: string[]): IHandValue => {
     },
     { card: "", count: 0 },
   )
+
+  return { numberOfMatches, mostMatched }
+}
+
+const getCardStrength = (card: string, useJokers = false) => {
+  if (card === "J" && useJokers) {
+    return 1
+  }
+
+  return cardStrengthsMap[card] || +card
+}
+
+const evaluateHand = (cards: string[]): IHandValue => {
+  const { numberOfMatches, mostMatched } = getNumberOfMatches(cards)
+
   const cardStrengths = cards.map((card) => cardStrengthsMap[card] || +card)
 
   if (mostMatched.count > 3) {
@@ -66,28 +89,13 @@ const evaluateHand = (cards: string[]): IHandValue => {
 
   return {
     firstEvalStrength: 1,
-    secondEvalStrengths: cards.map(
-      ([card, count]) => cardStrengthsMap[card] || +card,
-    ),
+    secondEvalStrengths: cardStrengths,
   }
 }
 
-export const processInput = (input: string[]) => {
-  return input.map((line) => {
-    const [cards, bid] = line.split(" ")
-    return { cards: cards.split(""), bid: Number(bid) }
-  })
-}
-
-export const partOne = (hands: IHand[]) => {
-  const evaluatedHands = hands.map((hand) => {
-    return {
-      ...hand,
-      value: evaluateHand(hand.cards),
-    }
-  })
-
-  evaluatedHands.sort((a, b) => {
+const sortCardStrengths =
+  (useJoker = false) =>
+  (a: IEvaluatedHand, b: IEvaluatedHand) => {
     if (a.value.firstEvalStrength > b.value.firstEvalStrength) {
       return 1
     }
@@ -107,13 +115,103 @@ export const partOne = (hands: IHand[]) => {
       }
     }
     return winner
+  }
+
+export const processInput = (input: string[]) => {
+  return input.map((line) => {
+    const [cards, bid] = line.split(" ")
+    return { cards: cards.split(""), bid: Number(bid) }
   })
+}
+
+export const partOne = (hands: IHand[]) => {
+  const evaluatedHands = hands.map((hand) => {
+    return {
+      ...hand,
+      value: evaluateHand(hand.cards),
+    }
+  })
+
+  evaluatedHands.sort(sortCardStrengths())
 
   return evaluatedHands.reduce((acc, curr, index) => {
     return acc + curr.bid * (index + 1)
   }, 0)
 }
 
-export const partTwo = (input: string[]) => {
-  return ""
+const evaluateHandWithJokers = (cards: string[]) => {
+  const { numberOfMatches, mostMatched } = getNumberOfMatches(cards)
+  let mostMatchedCount = mostMatched.count
+  const hasJoker = cards.includes("J")
+
+  const cardStrengths = cards.map((card) => getCardStrength(card, true))
+
+  if (mostMatched.card !== "J" && mostMatched.count > 1) {
+    mostMatchedCount += numberOfMatches.get("J") || 0
+  }
+
+  if (mostMatched.card === "J" && mostMatched.count < 5) {
+    mostMatchedCount +=
+      [...numberOfMatches.entries()]
+        .filter(([card]) => card !== "J")
+        .reduce((acc, [, curr]) => (acc < curr ? curr : acc), 0) || 1
+  }
+
+  if (mostMatchedCount > 3) {
+    return {
+      firstEvalStrength: mostMatchedCount + 2,
+      secondEvalStrengths: cardStrengths,
+    }
+  }
+
+  if (mostMatchedCount === 3) {
+    const isFullHouse =
+      [...numberOfMatches.entries()].filter(
+        ([card, count]) => count === 2 && card !== mostMatched.card,
+      ).length === 1
+    return {
+      firstEvalStrength: isFullHouse ? 5 : 4,
+      secondEvalStrengths: cardStrengths,
+    }
+  }
+  const numberOfPairs = [...numberOfMatches.values()].filter(
+    (match) => match === 2,
+  ).length
+  if (numberOfPairs === 2) {
+    return {
+      firstEvalStrength: 3,
+      secondEvalStrengths: cardStrengths,
+    }
+  }
+  if (numberOfPairs === 1) {
+    return {
+      firstEvalStrength: 2,
+      secondEvalStrengths: cardStrengths,
+    }
+  }
+  if (cards.includes("J")) {
+    return {
+      firstEvalStrength: 2,
+      secondEvalStrengths: cardStrengths,
+    }
+  }
+  return {
+    firstEvalStrength: 1,
+    secondEvalStrengths: cardStrengths,
+  }
+}
+
+export const partTwo = (hands: IHand[]) => {
+  const evaluatedHands = hands.map((hand) => {
+    return {
+      ...hand,
+      value: evaluateHandWithJokers(hand.cards),
+    }
+  })
+
+  evaluatedHands.sort(sortCardStrengths(true))
+
+  return evaluatedHands.reduce((acc, curr, index) => {
+    return acc + curr.bid * (index + 1)
+  }, 0)
 }
