4b3a0b54344386efed3e8127ebebcd7e93bd364f
Aleksandar Stolikj <stolaaar@gmail.com>
2024-12-20 23:39:03 +0100

    feat: solve day 18 part 1

---
+++ golang/2024/day-18/main.go
@@ -0,0 +1,171 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+)
+
+type Coordinate struct {
+	Corrupted    bool
+	ShortestPath float64
+	Y, X         int
+}
+
+type Direction int
+
+const (
+	Up Direction = iota
+	Down
+	Left
+	Right
+)
+
+var (
+	OutOfMap    = fmt.Errorf("OutOfMap")
+	Obstruction = fmt.Errorf("Obstruction")
+)
+
+var directions map[Direction][2]int = map[Direction][2]int{
+	Up:    {-1, 0},
+	Down:  {1, 0},
+	Left:  {0, -1},
+	Right: {0, 1},
+}
+
+type RAM struct {
+	grid [][]*Coordinate
+}
+
+func createRamGrid(n int) *RAM {
+	grid := make([][]*Coordinate, n)
+
+	for i := range grid {
+		grid[i] = make([]*Coordinate, n)
+
+		for j := range grid[i] {
+			grid[i][j] = &Coordinate{
+				ShortestPath: math.Inf(0),
+				Y:            i,
+				X:            j,
+			}
+		}
+	}
+
+	return &RAM{
+		grid: grid,
+	}
+}
+
+func (r *RAM) placeCorruptedCoordinates(coordinates []string) {
+	for _, coordinate := range coordinates {
+		parts := strings.Split(coordinate, ",")
+
+		x, _ := strconv.Atoi(parts[0])
+		y, _ := strconv.Atoi(parts[1])
+
+		r.grid[y][x].Corrupted = true
+	}
+}
+
+func (r *RAM) explore(coordinate *Coordinate) {
+	path := coordinate.ShortestPath + 1
+
+	y, x := coordinate.Y+directions[Up][0], coordinate.X+directions[Up][1]
+
+	if (y >= 0 && y < len(r.grid)) && (x >= 0 && x < len(r.grid[0])) {
+		if !r.grid[y][x].Corrupted && r.grid[y][x].ShortestPath > path {
+			r.grid[y][x].ShortestPath = path
+
+			r.explore(r.grid[y][x])
+		}
+	}
+
+	y, x = coordinate.Y+directions[Down][0], coordinate.X+directions[Down][1]
+
+	if (y >= 0 && y < len(r.grid)) && (x >= 0 && x < len(r.grid[0])) {
+		if !r.grid[y][x].Corrupted && r.grid[y][x].ShortestPath > path {
+			r.grid[y][x].ShortestPath = path
+
+			r.explore(r.grid[y][x])
+		}
+	}
+
+	y, x = coordinate.Y+directions[Left][0], coordinate.X+directions[Left][1]
+
+	if (y >= 0 && y < len(r.grid)) && (x >= 0 && x < len(r.grid[0])) {
+		if !r.grid[y][x].Corrupted && r.grid[y][x].ShortestPath > path {
+			r.grid[y][x].ShortestPath = path
+
+			r.explore(r.grid[y][x])
+		}
+	}
+
+	y, x = coordinate.Y+directions[Right][0], coordinate.X+directions[Right][1]
+
+	if (y >= 0 && y < len(r.grid)) && (x >= 0 && x < len(r.grid[0])) {
+		if !r.grid[y][x].Corrupted && r.grid[y][x].ShortestPath > path {
+			r.grid[y][x].ShortestPath = path
+
+			r.explore(r.grid[y][x])
+		}
+	}
+}
+
+func ProcessInput(input []string) interface{} {
+	return input[:len(input)-1]
+}
+
+func (r *RAM) reset(x, y int) {
+	for i, row := range r.grid {
+		for j, col := range row {
+			if i == 0 && j == 0 || (i < y) {
+				continue
+			}
+			if !col.Corrupted {
+				r.grid[i][j].ShortestPath = math.Inf(0)
+			}
+		}
+	}
+}
+
+func PartOne(input interface{}) interface{} {
+	ram := createRamGrid(71)
+
+	ram.placeCorruptedCoordinates(input.([]string)[:1024])
+
+	ram.grid[0][0].ShortestPath = 0
+
+	ram.explore(ram.grid[0][0])
+
+	return ram.grid[70][70].ShortestPath
+}
+
+func PartTwo(input interface{}) interface{} {
+	ram := createRamGrid(71)
+
+	ram.placeCorruptedCoordinates(input.([]string)[:1024])
+	ram.grid[0][0].ShortestPath = 0
+
+	tests := input.([]string)[1024:]
+
+	for _, test := range tests {
+		parts := strings.Split(test, ",")
+
+		x, _ := strconv.Atoi(parts[0])
+		y, _ := strconv.Atoi(parts[1])
+
+		ram.grid[y][x].Corrupted = true
+
+		ram.reset(x, y)
+
+		ram.explore(ram.grid[0][0])
+
+		if ram.grid[70][70].ShortestPath == math.Inf(0) {
+			return fmt.Sprintf("%d,%d", x, y)
+		}
+	}
+
+	return ram.grid[70][70].ShortestPath
+}
